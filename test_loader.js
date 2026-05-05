7e1c01886a4f5dc69da8241d0318e37a721354c3
ocrespof <ocrespof@unemi.edu.ec>
2026-05-04 19:31:47 -0500

    Delete test_loader.js

--- test_loader.js
+++
@@ -1,8 +0,0 @@
-import { pathToFileURL } from 'url';
-import path from 'path';
-
-const file = path.resolve('./cmds/juegos/trivia.js');
-const imported = await import(pathToFileURL(file).href);
-const pluginObj = { ...imported };
-console.log('pluginObj.before:', typeof pluginObj.before);
-console.log('imported.before:', typeof imported.before);
